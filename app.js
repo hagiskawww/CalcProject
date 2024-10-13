c6351664942cfaee18e202fbb6e266bfcf26d9f5
Vlad <v.zhurabetmen@gmail.com>
2024-10-13 20:08:52 +0300

    calc

--- app.js
+++ app.js
@@ -1,100 +1,58 @@
-// let num = 42
-// let firstName = 'Vlad'
-// const isProgrammer = true
-//
-// /* Can Do
-//  let $ = 'test'
-//  let $num = 42
-//  let num$ = 42
-//  let _ = 49
-//  let _num = 12
-//  let num_ = 12
-//  let first_name = 'Elena' // bad
-//  let myNum = 34 // good
-//  let num42 = 10
-//
-//  firstName = 'Max'
-//  isProgrammer = false // error because of const *
-//
-// // Restricted
-// // let 42num = '11'
-// // let ny-num = 18
-// // let false = 23
-// // alert(firstName)
-// // console.log( 'Test:', firstName, isProgrammer, num)
-// // console.log(num + 10)
-// // console.log(num - 10)
-// // console.log(num * 10)
-// // console.log(num / 10)
-// // console.log(num)
-// //
-// // let num2 = num + 10
-// // console.log(num)
-// // console.log(num2)
-// // num = num2 - num
-// // console.log(num, num2)
-// //
-// // let num3 = (num + 10) * 2 / (5 - 1)
-// //  console.log(num3) */
-//
-// // const fullName = firstName + ' Zhuravitsky'
-// // const fullName = firstName + ' ' + 'Zhuravitsky'
-// // console.log(fullName)
-//
-// const resultElement = document.getElementById('result')
-// const input1 = document.getElementById('input1')
-// const input2 = document.getElementById('input2')
-// const submitButton = document.getElementById('submit')
-// const submitPlus = document.getElementById('plus')
-// const submitMinus = document.getElementById('minus')
-// let action = '+'
-//
-//
-// // console.log(resultElement.textContent)
-// // resultElement.textContent = 42
-//
-// // console.log(sum)
-//
-// submitPlus.onclick = function () {
-//     action = '+'
-// }
-//
-// submitMinus.onclick = function () {
-//     action = '-'
-// }
-//
-// function printResult (result) {
-//     if (result < 0) {
-//         resultElement.style.backgroundColor = "red"
-//     } else if (result > 0) {
-//         resultElement.style.backgroundColor = "green"
-//     }
-//     resultElement.textContent = result
-// }
-//
-// function computeNumbersWithAction (inp1, inp2, actionSymbol) {
-//     const num1 = Number(inp1.value)
-//     const num2 = Number(inp2.value)
-//
-//     // if (actionSymbol == '+') {
-//     //     return num1 + num2
-//     // } else if (actionSymbol == '-') {
-//     //     return num1 - num2
-//     // }
-//
-//     return actionSymbol == '+' ? num1 + num2 : num1 - num2
-// }
-//
-// submitButton.onclick = function () {
-//     const result = computeNumbersWithAction(input1, input2, action)
-//     printResult(result)
-//     // if (action == '+') {
-//     //     const sum = Number(input1.value) + Number(input2.value)
-//     //     printResult(sum)
-//     // } else if (action == '-') {
-//     //     const sum = Number(input1.value) - Number(input2.value)
-//     //     printResult(sum)
-//     // }
-//
-// }
+const resultEl = document.getElementById('result')
+const input1 = document.getElementById('input1')
+const input2 = document.getElementById('input2')
+const plusBtn = document.getElementById('plus')
+const minusBtn = document.getElementById('minus')
+const multiplyBtn = document.getElementById('multiply')
+const divideBtn = document.getElementById('divide')
+const submitBtn = document.getElementById('submit')
+let action = '+'
+
+plusBtn.onclick = function () {
+    action = '+'
+}
+
+minusBtn.onclick = function () {
+    action = '-'
+}
+
+multiplyBtn.onclick = function () {
+    action = '*'
+}
+
+divideBtn.onclick = function () {
+    action = '/'
+}
+
+function printResult (result) {
+    if (result < 0) {
+        result.style.color = "red"
+    } else if (result > 0) {
+        result.style.color = "green"
+    }
+
+    resultEl.textContent = result.value
+}
+
+function calc (inp1, inp2, asc) {
+    const num1 = Number(inp1.value)
+    const num2 = Number(inp2.value)
+
+    if (asc == '+') {
+        return num1 + num2
+    } else if (asc == '-') {
+        return num1 - num2
+    } else if (asc == '*') {
+        return num1 * num2
+    } else if (asc == '/') {
+        return num1 / num2
+    }
+
+}
+
+submitBtn.onclick = function calcResult () {
+    const result = calc (input1, input2, action)
+    printResult (resultEl.textContent = result)
+}
+
 
